eee19f7dd572d85d09c9525c49713b64cbe0d016
jamie reaves <jamiereaves@utexas.edu>
2018-06-27 17:25:14 -0500

    polished up comments and readme and am submit ready. now to work on beanazonManager

--- beanazonCustomer.js
+++ beanazonCustomer.js
@@ -25,18 +25,22 @@ connection.connect(function(err){
     //run the queryBeans function
     queryBeans();
 });
-
+//function that generates the table that displays the selection of beans. contains a call for the function that prompts the user selection.
 function queryBeans(){
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         resLength = res.length + 1;
+        //clear global values
         iDarray = [];
         stockCounter = [];
         productChoices =[];
         priceList = [];
+        //display for information gathered through mysql server query
         console.log("================================================================================================================");
         console.log("                      WELCOME TO BEANAZON, THE INTERNET'S PREMIER BEAN RETAILER")
         console.log("================================================================================================================");
+        //added code to make sure 1 and 2 digit item id's display uniformly to ease user interface. could be expanded if a sufficient 
+        //number of items are added to generate 3 digit id's. these for loops also populate arrays that hold product data for use in the next function.
         if (res.length > 9){
         for (var i = 0; i < 9; i++){
             console.log("beanazon product ID: " + res[i].item_id + "  || " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " per kg | quantity available: " + res[i].stock_quantity + " kg");
@@ -54,10 +58,11 @@ function queryBeans(){
             }
         } 
         console.log("================================================================================================================");
+        //start function is called
         start();
     })  
 }
-
+//start function prompts the user for input and uses that input to update stock (if order is possible)
 function start() {
     inquirer
         .prompt([
@@ -65,6 +70,7 @@ function start() {
                 name: "itemID",
                 type: "input",
                 message: "Please enter the ID of the bean you'd like to purchase.",
+                //make sure entry is a positive integer in the range of possible ids
                 validate: function(value) {
                     if (isNaN(value) === false && value > 0 && value < resLength) {
                       return true;
@@ -77,6 +83,7 @@ function start() {
                 name: "quantity",
                 type: "input",
                 message: "Please enter the quanity you'd like to purchase.",
+                //make sure entry is a positive integer
                 validate: function(value) {
                     if (isNaN(value) === false && value >= 1) {
                       return true;
@@ -87,23 +94,27 @@ function start() {
             }
         ])
         .then(function(answer) {
+            //clear selectionArray
             selectionArray = [];
+            //populate selectionArray with relevant data from the user's choice. these values are used in subsequent code.
             for (i=0; i < iDarray.length; i++) {
                 if (parseInt(iDarray[i]) === parseInt(answer.itemID)) {
                     selectionArray.push(parseInt(iDarray[i]));
                     selectionArray.push(productChoices[i]);
                     selectionArray.push(parseInt(stockCounter[i]));
+                    //used parseFloat since the price needs to have 2 decimal values
                     selectionArray.push(parseFloat(priceList[i]));
                 }
             }
-
+            //ensure there is enough stock on hand to fulfill the order. if there isn't, notify user.
             if (parseInt(answer.quantity) > parseInt(selectionArray[2])) {
                 console.log("Sorry, we don't have enough " + selectionArray[1].toLowerCase() + " in stock to fulfill your order");
+                //call function that allows user to choose to continue or exit the application.
                 endgame();
             }
             
             else{
-
+            //if stock is sufficient, update the relevant stock_quantity from the mysql table.
               connection.query(
                 "UPDATE products SET ? WHERE ?",
                 [
@@ -116,11 +127,15 @@ function start() {
                 ],
                 function(err) {
                   if (err) throw err;
+                  //set variable to hold the total cost of the order
                   var cost = selectionArray[3] * parseInt(answer.quantity);
+                  //update the stock in the selectionArray. this prevents the user from being allowed to order more than is actually on hand.
                   selectionArray[2] = parseInt(selectionArray[2]) - parseInt(answer.quantity);
+                  //inform user of the success of the order and give them a summary of the relevant order details.
                   console.log("You have purchased " + answer.quantity + " kg of " + selectionArray[1].toLowerCase() + 
                   " & your account has been charged $" + cost.toFixed(2) + 
                   ". Thanks for shopping with beanazon!!");
+                  //call function that allows user to choose to continue or exit the application.
                   endgame();
                 }
               );
@@ -129,10 +144,11 @@ function start() {
           
         )};
 
-
+//function that allows user to either place another order, or exit the app
 function endgame() {
     inquirer
-        .prompt({
+      //ask if the user would like to continue via a y or n prompt  
+      .prompt({
         name: "continue",
         type: "confirm",
         message: "Would you like to order more beans?",
